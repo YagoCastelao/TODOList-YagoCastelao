8c370ad64eb9d5602d6a912894976e1bea42804e
Yago Cima Castelao <yago_castelau@hotmail.com>
2024-12-12 03:52:11 +0100

    inicio del proyecto

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,14 +1,33 @@
 import { useState } from 'react'
-import './App.css'
+
+
+const lista = [{
+  tarea: "lavar Louça",
+  hora: "12:40"
+}]
 
 function App() {
-  
+  const [state, setState] = useState(lista)
+
+  const agregar = () => {
+    setState([...state, {tarea: "Tarea"}])
+  }
 
   return (
     <>
-      
+        <h1>My TODO List</h1>
+          <button onClick={agregar}>Agregar Tarea</button>
+          <ul>
+            {state.map((item) => {
+              return (<>
+                <li>
+                  {item.tarea}
+                </li>
+              </>)
+            })}
+          </ul>
     </>
   )
 }
 
-export default App
+export default App  
